349370a161dbf9d0c8d194dff7e0c439d54d4392
Diogo Dumas <diogodumas78@gmail.com>
2019-03-13 16:56:30 -0300

    aula 15

--- src/main/java/com/diogo/cursomc/CurscomcApplication.java
+++ src/main/java/com/diogo/cursomc/CurscomcApplication.java
@@ -1,13 +1,32 @@
 package com.diogo.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CurscomcApplication {
+import com.diogo.cursomc.domain.Categoria;
+import com.diogo.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CurscomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CurscomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
